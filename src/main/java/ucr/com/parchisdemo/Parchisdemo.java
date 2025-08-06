f76e65eb3a930a0bacc61786e8340a73c713603c
tm3100 <tm3100@gmail.com>
2025-08-06 17:04:14 -0600

    Creación del proyecto

---
+++ src/main/java/ucr/com/parchisdemo/Parchisdemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.com.parchisdemo;
+
+/**
+ *
+ * @author ITM
+ */
+public class Parchisdemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
